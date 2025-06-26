714c8141ad3962849e45c469f3a33123681365a8
Jonas Jönsson <berglund.jonas@gmail.com>
2025-06-26 22:49:55 +0200

    v4

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
