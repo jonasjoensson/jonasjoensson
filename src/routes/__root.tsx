787d91b8028fecfc1fe05e08b25d33ff7f6e0702
Jonas Jönsson <berglund.jonas@gmail.com>
2025-06-30 13:22:48 +0200

    feat: Add Dark Mode

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router"
 import { Analytics } from "@vercel/analytics/react"
+import { ModeToggle } from "@/components/mode-toggle"
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <ModeToggle />
       <main>
         <Outlet />
         <Analytics />
